22ee112d897f42d9a4680352a178f97fc189bacf
Hannah Hudson <hannah.hch@gmail.com>
2017-08-31 13:34:11 -0400

    changed port for heroku

--- app.js
+++ app.js
@@ -115,6 +115,6 @@ app.post('/:id/delete', function(req,res){
 
 module.exports = app;
 
-app.listen(8080, function(){
-  console.log("Listening on port 3000!")
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
