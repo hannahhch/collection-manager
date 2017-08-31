04001e17dbdd3c86948bda6219414b38d9b173a2
Hannah Hudson <hannah.hch@gmail.com>
2017-08-31 14:09:06 -0400

    did some thing for heroku, still does not deploy. able to run localy with heroku local

--- app.js
+++ app.js
@@ -115,6 +115,6 @@ app.post('/:id/delete', function(req,res){
 
 module.exports = app;
 
-server.listen(process.env.PORT || 8000, function(){
+app.listen(process.env.PORT || 8000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
